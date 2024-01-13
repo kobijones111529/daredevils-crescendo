4051f95fb929215ada21ec435290deb4b8d97b2b
Kobi Jones <kobijonesmail@gmail.com>
2024-01-13 09:22:40 -0600

    Add basic drive commands in Scala

--- src/main/scala/frc/robot/scala/robot/Container.scala
+++ src/main/scala/frc/robot/scala/robot/Container.scala
@@ -1,16 +1,24 @@
 package frc.robot.scala.robot
 
+import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController
 import frc.robot.scala.robot.subsystems.comp.Drivetrain
+
+import scala.math.*
 
 class Container:
   val auto: Option[Command] = None
-  
+
   private val drivetrain: Drivetrain = Drivetrain(config.drivetrain)
-  
+
+  private val xbox: CommandXboxController = CommandXboxController(
+    config.driverStation.xbox
+  )
+
   configureBindings()
-  
+
   private def configureBindings(): Unit =
-    drivetrain.setDefaultCommand(drivetrain.run(() => drivetrain.arcadeDrive(???, ???)))
-  
-end Container
+    drivetrain.setDefaultCommand(
+      commands.drive.arcade(drivetrain, xbox.getLeftY, xbox.getRightX)
+    )
--- src/main/scala/frc/robot/scala/robot/config/package.scala
+++ src/main/scala/frc/robot/scala/robot/config/package.scala
@@ -3,6 +3,9 @@ package frc.robot.scala.robot
 import frc.robot.scala.robot.subsystems.comp.Drivetrain
 
 package object config:
+  object driverStation:
+    val xbox: Int = 0
+  
   val drivetrain: Drivetrain.Config = Drivetrain.Config(
     left = Drivetrain.Config.DriveGroup(
       primaryID = can.drivetrain.left.primary,

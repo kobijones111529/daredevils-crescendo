13ce7078cc3706d97e58c47f29b846aa60f58a73
Kobi Jones <kobijonesmail@gmail.com>
2024-01-27 23:24:53 -0600

    Remove scala module

--- src/main/scala/frc/robot/scala/Main.scala
+++
@@ -1,8 +0,0 @@
-package frc.robot.scala
-
-import edu.wpi.first.wpilibj.RobotBase
-import frc.robot.scala.robot.Instance as Robot
-
-object Main:
-  def main(args: Array[String]): Unit =
-    RobotBase.startRobot(() => Robot)
--- src/main/scala/frc/robot/scala/robot/subsystems/SimpleDifferentialDrive.scala
+++
@@ -1,8 +0,0 @@
-package frc.robot.scala.robot.subsystems
-
-import scala.annotation.unused
-
-trait SimpleDifferentialDrive[Drivetrain]:
-  extension (@unused drivetrain: Drivetrain) def stop(): Unit
-  extension (@unused drivetrain: Drivetrain) def tankDrive(left: Double, right: Double): Unit
-  extension (@unused drivetrain: Drivetrain) def arcadeDrive(move: Double, turn: Double): Unit
